875970f185641f2d4a424bd323e281352ecb243d
Caio Carvalho <caiocarvalho.py@gmail.com>
2021-03-10 15:01:11 -0400

    try e except

--- example.py
+++ example.py
@@ -1,12 +1,18 @@
 def dividir(dividendo, divisor):
+    # print(divisor.resultado)
     return dividendo / divisor
 
 
 def testa_divisao(divisor):
-    resultado = dividir(10, divisor)
-    print(f'O resultado da divisão de 10 por {divisor} é {resultado}')
+    try:
+        resultado = dividir(10, divisor)
+        print(f'O resultado da divisão de 10 por {divisor} é {resultado}')
+    except ZeroDivisionError:
+        print('Erro de divisão por zero tratado')
 
 try:
     testa_divisao(0)
-except ZeroDivisionError as e:
-    print(f'Não é possível realizar a divisão por zero: {e}')
+except AttributeError:
+    print('Erro de atributo tratado')
+
+print('Programa encerrado')
