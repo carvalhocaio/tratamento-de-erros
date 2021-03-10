f602a48ef634bc534814ddce8ff7bbeb6a870d23
Caio Carvalho <caiocarvalho.py@gmail.com>
2021-03-10 17:39:19 -0400

    simulando atributos privados

--- main.py
+++ main.py
@@ -10,12 +10,49 @@ class ContaCorrente:
     taxa_operacao = None
 
     def __init__(self, cliente, agencia, numero):
-        self.saldo = 100
+        self.__saldo = 100
         self.cliente = cliente
-        self.agencia = agencia
-        self.numero = numero
+        self.__set_agencia(agencia)
+        self.__numero = numero
         ContaCorrente.total_contas_criadas += 1
         ContaCorrente.taxa_operacao = 30 / ContaCorrente.total_contas_criadas
+
+    @property
+    def agencia(self):
+        return self.__agencia
+
+    def __set_agencia(self, value):
+        if not isinstance(value, int):
+            return
+        if value <= 0:
+            print('O atributo agencia deve ser maior que zero')
+            return
+
+        self.__agencia = value
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    def __set_numero(self, value):
+        if not isinstance(value, int):
+            return
+        if value <= 0:
+            return
+
+        self.__numero = value
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @saldo.setter
+    def saldo(self, value):
+        if not isinstance(value, int):
+            return
+        if value <= 0:
+            return
+        self.__saldo = value
 
     def transferir(self, valor, favorecido):
         favorecido.depositar(valor)
@@ -27,4 +64,6 @@ class ContaCorrente:
         self.saldo += valor
 
 
-conta_corrente = ContaCorrente(None, '00', '101')
+conta_corrente = ContaCorrente(None, 'agencia falsa', 22500)
+conta_corrente.agencia = 0
+print(conta_corrente.agencia)
