f8b8d33212e79548440ef33274b684f4d2aa77d1
Caio Carvalho <caiocarvalho.py@gmail.com>
2021-03-10 16:34:22 -0400

    raise

--- example.py
+++ example.py
@@ -1,18 +1,32 @@
 def dividir(dividendo, divisor):
-    # print(divisor.resultado)
-    return dividendo / divisor
+    if not (isinstance(dividendo, int) and isinstance(divisor, int)):
+        raise ValueError('dividir() deve receber apenas argumentos inteiros')
+
+    try:
+        aux = dividendo / divisor
+    except:
+        print(f'Não foi possível dividir {dividendo} por {divisor}')
+        raise
+    return aux
 
 
 def testa_divisao(divisor):
-    try:
-        resultado = dividir(10, divisor)
-        print(f'O resultado da divisão de 10 por {divisor} é {resultado}')
-    except ZeroDivisionError:
-        print('Erro de divisão por zero tratado')
+    resultado = dividir(10, divisor)
+    print(f'O resultado da divisão de 10 por {divisor} é {resultado}')
+
+# try:
+#     testa_divisao(2.5)
+# except ZeroDivisionError as e:
+#     print('Erro de divisão por zero')
+# # except Exception as e:
+# #     print('tratamento genérico')
+
+# print('Programa encerrado')
+
 
 try:
-    testa_divisao(0)
-except AttributeError:
-    print('Erro de atributo tratado')
-
-print('Programa encerrado')
+    print('o fluxo está aqui')
+    raise ValueError
+except Exception:
+    print('Agora ele foi pra cá')
+print('E enfim ele continua...')
